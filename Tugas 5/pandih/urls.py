69b3c52948d792e74fef6494e3bd27b970b79a93
X3RON3 <65886784+X3RON3@users.noreply.github.com>
2024-06-25 20:44:13 +0700

    Add files via upload

---
+++ Tugas 5/pandih/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from pandih import views
+
+app_name = 'pandih'
+urlpatterns = [
+   path('', views.readStudent, name='read-data-student'),   
+   path('create/', views.createStudent, name='create-data-student'),   
+   path('update/<str:id>', views.updateStudent, name='update-data-student'),   
+   path('delete/<str:id>', views.deleteStudent, name='delete-data-student'),
+   path('update/updaterecord/<str:id>', views.updaterecord, name='updaterecord'),
+]
